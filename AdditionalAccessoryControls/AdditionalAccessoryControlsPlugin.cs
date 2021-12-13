6c3a2fc5a8c2c5cc80fd4b1ec8d65dcf0dbaf15b
OrangeSpork <57274555+OrangeSpork@users.noreply.github.com>
2021-12-12 18:04:47 -0700

    Version bump

--- AdditionalAccessoryControls/AdditionalAccessoryControlsPlugin.cs
+++ AdditionalAccessoryControls/AdditionalAccessoryControlsPlugin.cs
@@ -28,7 +28,7 @@ namespace AdditionalAccessoryControls
 
         public const string GUID = "orange.spork.additionalaccessorycontrolsplugin";
         public const string PluginName = "Additional Accessory Controls";
-        public const string Version = "1.2.2";
+        public const string Version = "1.2.3";
 
         public static AdditionalAccessoryControlsPlugin Instance { get; set; }  // Me
 
